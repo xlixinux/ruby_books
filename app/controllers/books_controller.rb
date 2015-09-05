ea1ac8d31f9b11ab6fcd6ed96effaa50ed77ceef
Xinke Li <xlixinux@gmail.com>
2015-09-05 01:11:16 -0400

    Finish connecting MVC

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,5 @@
 class BooksController < ApplicationController
 	def index
-		@books = ["Programming Ruby",
-						  "Well-Grounded Rubyist",
-						  "Ruby for Rails",
-						  "The Ruby Programming Language"]
+		@books = Book.all
 	end
 end
