dc590b8b8ab599ba113f1ada003dd92f5737b04b
Xinke Li <xlixinux@gmail.com>
2015-09-06 01:06:32 -0400

    Finished adding a migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905040741) do
+ActiveRecord::Schema.define(version: 20150906043923) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20150905040741) do
     t.string   "rating"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "released_on"
+    t.text     "description"
   end
 
 end
