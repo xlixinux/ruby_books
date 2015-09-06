b0e9d6817ee3e85faedf4b840a78028fc2000ff1
Xinke Li <xlixinux@gmail.com>
2015-09-06 13:56:39 -0400

    Finish adding routes

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,9 @@
 module BooksHelper
+	def format_price(book)
+		if book.price > 20
+			content_tag(:strong, "#{book.price} is Expensive!")
+		else
+			number_to_currency(book.price)
+		end
+	end
 end
